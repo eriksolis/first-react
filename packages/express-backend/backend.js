d2d888dfb0f4aebaa1e2538e00e14812ba6769fb
eriksolis <eriksolisschool@gmail.com>
2024-10-14 13:14:26 -0700

    IE3: Part 3

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,5 +1,6 @@
 // backend.js
 import express from "express";
+import cors from "cors";
 
 const app = express();  
 const port = 8000;
@@ -57,6 +58,8 @@ const removeUser = (user) => {
   const indexOfUser = users["users_list"].indexOf(user);
   users["users_list"].splice(indexOfUser, 1);
 };
+
+app.use(cors());
 
 app.use(express.json());
 
